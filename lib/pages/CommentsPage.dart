a1a198bbbdfb08b4205388714b71571ff903b89b
Alphin Roy <alphinroy430@gmail.com>
2020-07-26 02:09:35 +0530

    notification

--- lib/pages/CommentsPage.dart
+++ lib/pages/CommentsPage.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:Capturoca/widgets/HeaderWidget.dart';
 import 'package:Capturoca/widgets/ProgressWidget.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -9,37 +7,38 @@ import 'package:Capturoca/pages/HomePage.dart';
 import 'package:timeago/timeago.dart' as tAgo;
 
 class CommentsPage extends StatefulWidget {
-
   final String postId;
   final String postOwnerId;
   final String postImageUrl;
-  
 
-  CommentsPage({this.postId,this.postOwnerId,this.postImageUrl});
+  CommentsPage({this.postId, this.postOwnerId, this.postImageUrl});
 
   @override
-  CommentsPageState createState() => CommentsPageState(postId: postId,postOwnerId: postOwnerId,postImageUrl: postImageUrl);
+  CommentsPageState createState() => CommentsPageState(
+      postId: postId, postOwnerId: postOwnerId, postImageUrl: postImageUrl);
 }
 
 class CommentsPageState extends State<CommentsPage> {
-
   final String postId;
   final String postOwnerId;
   final String postImageUrl;
   TextEditingController commentTextEditingController = TextEditingController();
 
-  CommentsPageState({this.postId,this.postOwnerId,this.postImageUrl});
-   
-  retrieveComments(){
+  CommentsPageState({this.postId, this.postOwnerId, this.postImageUrl});
+
+  retrieveComments() {
     return StreamBuilder(
-      stream: commentsReference.document(postId).collection("comment").orderBy("timestamp",descending: false).snapshots(),
-      builder: (context,dataSnapshot){
-        if(!dataSnapshot.hasData)
-        {
+      stream: commentsReference
+          .document(postId)
+          .collection("comment")
+          .orderBy("timestamp", descending: false)
+          .snapshots(),
+      builder: (context, dataSnapshot) {
+        if (!dataSnapshot.hasData) {
           return circularProgress();
         }
-        List<Comment>comments = [];
-        dataSnapshot.data.documents.forEach((document){
+        List<Comment> comments = [];
+        dataSnapshot.data.documents.forEach((document) {
           comments.add(Comment.fromDocument(document));
         });
         return ListView(
@@ -49,56 +48,64 @@ class CommentsPageState extends State<CommentsPage> {
     );
   }
 
-  saveComment(){
+  saveComment() {
     commentsReference.document(postId).collection("comment").add({
       "username": currentUser.username,
-      "comment":commentTextEditingController.text,
+      "comment": commentTextEditingController.text,
       "timestamp": DateTime.now(),
       "url": currentUser.url,
-      "userId":currentUser.id
+      "userId": currentUser.id
     });
 
     bool isNotPostOwner = postOwnerId != currentUser.id;
-    if(isNotPostOwner){
+    if (isNotPostOwner) {
       activityFeedReference.document(postOwnerId).collection("feedItems").add({
         "type": "comment",
-        "commentDate": timestamp,
-        "postId":postId,
-        "userId":currentUser.id,
+        "commentData": commentTextEditingController.text,
+        "postId": postId,
+        "userId": currentUser.id,
         "username": currentUser.username,
         "userProfileImg": currentUser.url,
         "url": postImageUrl,
-
-        });
-        
-
+        "timestamp": timestamp
+      });
     }
     commentTextEditingController.clear();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: header(context,strTitle:"Comments"),
-      body: Column(children: <Widget>[Expanded(child: retrieveComments()),
-      Divider(),
-      ListTile(
-        title: TextFormField(controller: commentTextEditingController,
-        decoration: InputDecoration(
-          labelText: "Write Comment here",
-          labelStyle: TextStyle(color: Colors.black),
-          enabledBorder:  UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-          focusedBorder:  UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
-        ),
-        style: TextStyle(color:Colors.black),
-        ),
-        trailing: OutlineButton(onPressed: saveComment,
-          borderSide: BorderSide.none,
-          child: Text("Publish",style:TextStyle(color:Colors.lightGreenAccent,fontWeight:FontWeight.bold),),
-        ),
-      ),
-      ],
+      appBar: header(context, strTitle: "Comments"),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: retrieveComments()),
+          Divider(),
+          ListTile(
+            title: TextFormField(
+              controller: commentTextEditingController,
+              decoration: InputDecoration(
+                labelText: "Write Comment here",
+                labelStyle: TextStyle(color: Colors.black),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black)),
+              ),
+              style: TextStyle(color: Colors.black),
+            ),
+            trailing: OutlineButton(
+              onPressed: saveComment,
+              borderSide: BorderSide.none,
+              child: Text(
+                "Publish",
+                style: TextStyle(
+                    color: Colors.lightGreenAccent,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -111,37 +118,38 @@ class Comment extends StatelessWidget {
   final String comment;
   final Timestamp timestamp;
 
-  Comment({this.username,this.userId,this.url,this.comment,this.timestamp});
-  
+  Comment({this.username, this.userId, this.url, this.comment, this.timestamp});
 
-  factory Comment.fromDocument(DocumentSnapshot documentSnapshot){
+  factory Comment.fromDocument(DocumentSnapshot documentSnapshot) {
     return Comment(
       username: documentSnapshot["username"],
       userId: documentSnapshot["user"],
       url: documentSnapshot["url"],
       comment: documentSnapshot["comment"],
       timestamp: documentSnapshot["timestamp"],
-
-
     );
   }
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(bottom:6.0),
-    child: Container(
-      color:Colors.white,
-      child: Column(children: <Widget>[
-        ListTile(
-           title: Text(username +":  "+comment,style: TextStyle(color: Colors.black,fontSize:18.0),
-
-           ),
-           leading: CircleAvatar(
-             backgroundImage: CachedNetworkImageProvider(url),
-           ),
-           subtitle: Text(tAgo.format(timestamp.toDate()),style: TextStyle(color: Colors.black)),
-        ),
-      ],
-      )
-    ),);
+    return Padding(
+      padding: EdgeInsets.only(bottom: 6.0),
+      child: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  username + ":  " + comment,
+                  style: TextStyle(color: Colors.black, fontSize: 18.0),
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: CachedNetworkImageProvider(url),
+                ),
+                subtitle: Text(tAgo.format(timestamp.toDate()),
+                    style: TextStyle(color: Colors.black)),
+              ),
+            ],
+          )),
+    );
   }
 }
